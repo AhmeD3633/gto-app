b8512ee13c3e08e51242614a6f58b6f270b7d1d1
AhmeD3633 <143659754+AhmeD3633@users.noreply.github.com>
2024-12-28 22:45:49 +0200

    membership-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Hero from "../components/Hero/page";
 import GlobalTravel from "../components/GlobalTravel/page";
 import GtoNumbers from "../components/GtoNumbers/page";
-
+import MembershipBenefits from "../components/MembershipBenefits/page";
 export default function Page() {
   return (
     <div className="App">
       <Hero />
       <GlobalTravel />
       <GtoNumbers />
+      <MembershipBenefits />
     </div>
   );
 }
---
+++ src/components/MembershipBenefits/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import style from "./membership.module.css";
+import suite from "../../../public/assets/Images/suite.svg";
+import benefits from "../../../public/assets/Images/test.svg";
+import Image from "next/image";
+const page = () => {
+  return (
+    <div className={style.container}>
+      <div className={style.left}>
+        <Image src={suite} alt="suite" layout="responsive"/>
+      </div>
+      <div className={style.right}>
+        <Image src={benefits} alt="benefits" layout="responsive"/>
+      </div>
+    </div>
+  );
+};
+
+export default page;
