6141871eec5142136174be601eccf75296fbd8d7
AhmeD3633 <143659754+AhmeD3633@users.noreply.github.com>
2024-12-28 21:40:28 +0200

    GTO-in-numbers-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Hero/page";
-// import Global from "@/components/GlobalTravel/page";
 import GlobalTravel from "../components/GlobalTravel/page";
+import GtoNumbers from "../components/GtoNumbers/page";
+
 export default function Page() {
   return (
     <div className="App">
       <Hero />
       <GlobalTravel />
+      <GtoNumbers />
     </div>
   );
 }
---
+++ src/components/GtoNumbers/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import style from "./gto.module.css";
+
+const page = () => {
+  return (
+    <div className={style.container}>
+      <div className={style.content}>
+        <h2>GTO IN NUMBERS</h2>
+        <p>
+          More than <span style={{ color: "#d4a742" }}>1.2 </span>million happy
+          members by the end of 2023
+        </p>
+        <p>
+          More than<span style={{ color: "#d4a742" }}> £260 </span> million in
+          bookings by the end of 2024
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
